7a728d0ab671cc91f75c1e0b4b602ae58e9d0d1b
Raphaël Valyi <rvalyi@akretion.com>
2021-10-11 00:38:22 -0300

    xsdata --package nfelib.nfe.soap wsdl/nfe/*

---
+++ nfelib/nfe/soap/__init__.py
@@ -0,0 +1,15 @@
+from nfelib.nfe.soap.recepcaoevento4 import (
+    NfeRecepcaoEvento4SoapNfeRecepcaoEvento,
+    NfeRecepcaoEvento4SoapNfeRecepcaoEventoInput,
+    NfeRecepcaoEvento4SoapNfeRecepcaoEventoOutput,
+    NfeDadosMsg,
+    NfeResultMsg,
+)
+
+__all__ = [
+    "NfeRecepcaoEvento4SoapNfeRecepcaoEvento",
+    "NfeRecepcaoEvento4SoapNfeRecepcaoEventoInput",
+    "NfeRecepcaoEvento4SoapNfeRecepcaoEventoOutput",
+    "NfeDadosMsg",
+    "NfeResultMsg",
+]
